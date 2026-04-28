3b69c81397630c2a90887f763babf338682b854b
Calika <henriquessimon2004@gmail.com>
2026-04-28 20:50:59 -0300

    Código feito até agora

--- components/dayCard.js
+++ components/dayCard.js
@@ -6,61 +6,93 @@ export function DayCard(diaRotina) {
 
     if(diaRotina.meta) { //CASO O USUARIO JA TENHA PREENCHIDO A META
         meta = `
-            <label>Tempo Exercicio</label>
+            <h3>Tempo Exercicio</h3>
             <div class="divContadorMetaExercicio">
-                <span>Icon timer</span>
-                <div class="barraProgresso exercicioProgresso">
-                    <div class="progressoBarraExercicio"></div>
-                </div>
+                <span>Inicio</span>
+                <progress value="0" max="100"></progress>
             </div>
         `;
     } else { //CASO O USUARIO NÃO TENHA PREENCHIDO A META
         meta = `
             <div class="addMetaDia">
-                <label>Exercicio</label>
-                <div class="addMetaTreino">Adicionar meta de tempo de treino</div>
+                <h3>Exercício</h3>
+
+                <label>
+                    Tempo de treino
+                    <input type="time" class="fimTreino">
+                </label>
+
+                <progress value="0" max="100"></progress>
             </div>
         `;
     }
 
     if(diaRotina.hora) { //CASO O USUARIO JA TENHA SELECIONADO A HORA
         hora = `
-            <p>${diaRotina.hora}</p>
+            <time>${diaRotina.hora}</time>
         `
     } else { // CASO ELE NÃO TENHA SELECIONADO A HORA
         hora = `
-            <p class="inserirHoraInicioTreino">Inserir hora de inicio</p>
+            <label>Hora <input type="time"/></label>
         `
     }
 
 
     //RETORNAMOS O HTML DO CARD DO DIA FORMATADO CORRETAMENTE PARA INSERIR NA TELA
     return `
-        <div class="dayCard" style="background-color: ${diaRotina.color}">
-            <div class="headerDayCard divsCard">
-                <div>
+        <article class="dayCard" data-day="${diaRotina.dia}">
+            <header class="headerDayCard divsCard">
+                <div class="dateDiv">
                     <h2>${diaRotina.dia}</h2>
                     ${hora}
                 </div>
                 <div>
-                    <div>${diaRotina.status}</div>
+                    <span class="statusBox">${diaRotina.status}</span>
                 </div>
-            </div>
-            <div class="BodyDayCard divsCard">
-                <div class="meta exercicio" id="exercicioMetaCard">
-                    ${meta}
-                </div>
-                <div class="meta agua" id="aguaMetaCard">
-                    <label>Água bebida</label>
-                    <div class="barraProgresso aguaProgresso">
-                        <div class="progressoBarraAgua"></div>
+            </header>
+            <section class="BodyDayCard divsCard">
+                <section class="meta_exercicio" id="exercicioMetaCard">
+                    <div class="addMetaDia">
+                        
+                            <h3>Exercício</h3>
+                            <div>
+                                <label for="metaTreino">Tempo de treino</label>
+                                <input type="time" class="metaTreino" name="metaTreino">
+                            </div>
+                            <div>
+                                <progress value="0" max="100" class="barraTreino"></progress>
+                                <div>
+                                    <span class="contador">0h:0m:0s</span>
+                                    <button class="timerButton inicia">Inciar</button>
+                                </div>
+                            </div>
                     </div>
-                    <label>
-                        Mls bebidos hoje
-                        <input type="number" name="mls_bebidos_hoje" class="inserirMlAgua" id="inserirMlAgua" value=${diaRotina.meta_agua}/>
-                    <label/>
-                </div>
-            </div>
-        </div>
+                </section>
+                <section class="metaAguaSection" id="aguaMetaCard">
+                    <div>
+                        <label for="meta_agua">Meta de Água</label>
+                        <input 
+                            type="number" 
+                            name="meta_agua"
+                            class="inserirMetaAgua"
+                            value=0
+                        />
+                    </div>
+                    <div>
+                        <label for="mls_bebidos_hoje">Água bebida</label>
+                        <input 
+                            type="number" 
+                            name="mls_bebidos_hoje" 
+                            class="inserirMlAgua" 
+                            id="inserirMlAgua" 
+                            value=0
+                        />
+                    </div>
+                    <div class="barraProgresso aguaProgresso">
+                        <progress value="" max="100" class="barraAgua"></progress>
+                    </div>
+                </section>
+            </section>
+        </article>
     `;
 }
--- public/js/MainPage.js
+++ public/js/MainPage.js
@@ -10,9 +10,10 @@ const rotinaExercicios = [
         hora: 0,
         status: "Sem treino",
         meta_exercicio: 0,
-        meta_agua: metaDiariaAgua,
+        meta_agua: 400,
+        ml_bebidos: 0,
         tempo_feito: 0,
-        color: '#ff000079'
+        interval: null,
     },
     {
         dia: "Segunda",
@@ -20,8 +21,9 @@ const rotinaExercicios = [
         status: "Sem treino",
         meta_exercicio: 0,
         meta_agua: metaDiariaAgua,
+        ml_bebidos: 0,
         tempo_feito: 0,
-        color: '#ff582aab'
+        interval: null,
     },
     {
         dia: "Terça",
@@ -29,8 +31,9 @@ const rotinaExercicios = [
         status: "Sem treino",
         meta_exercicio: 0,
         meta_agua: metaDiariaAgua,
+        ml_bebidos: 0,
         tempo_feito: 0,
-        color: '#fbeb3e9c'
+        interval: null,
     },
     {
         dia: "Quarta",
@@ -38,8 +41,9 @@ const rotinaExercicios = [
         status: "Sem treino",
         meta_exercicio: 0,
         meta_agua: metaDiariaAgua,
+        ml_bebidos: 0,
         tempo_feito: 0,
-        color: '#4e9952a7'
+        interval: null,
     },
     {
         
@@ -48,8 +52,9 @@ const rotinaExercicios = [
         status: "Sem treino",
         meta_exercicio: 0,
         meta_agua: metaDiariaAgua,
+        ml_bebidos: 0,
         tempo_feito: 0,
-        color: '#576ec292'
+        interval: null,
     },
     {
         dia: "Sexta",
@@ -57,8 +62,9 @@ const rotinaExercicios = [
         status: "Sem treino",
         meta_exercicio: 0,
         meta_agua: metaDiariaAgua,
+        ml_bebidos: 0,
         tempo_feito: 0,
-        color: '#905bc2a7'
+        interval: null,
     },
     {
         dia: "Sabado",
@@ -66,10 +72,10 @@ const rotinaExercicios = [
         status: "Sem treino",
         meta_exercicio: 0,
         meta_agua: metaDiariaAgua,
+        ml_bebidos: 0,
         tempo_feito: 0,
-        color: '#fc92e5a0'
+        interval: null,
     }
-
 ]
 
 const cards = document.getElementById("cards"); //PEGA O ELEMENTO HTML PARA INSERIR CARDS DOS DIAS
@@ -80,10 +86,190 @@ rotinaExercicios.forEach(dia => {
     cards.innerHTML += DayCard(dia)
 })
 
+
+//ATIVA E DESATIVA CAIXA DE CONFIGURAÇÕES
+const configWrapper = document.getElementById('configWrapper')
+const configMenuBox = document.querySelector('.configMenuBox');
+
+//MOSTRA A CAIXA DE CONFIGS
+configWrapper.addEventListener('click', (e) => {
+    e.stopPropagation();
+    configMenuBox.classList.toggle('active')
+})
+
+//IMPEDE DE FECHAR AO CLICAR NELA MESMA OU ELEMENTO DENTRO DELA
+configMenuBox.addEventListener("click", (e) => {
+    e.stopPropagation();
+})
+
+//ESCONDE A CAIXA DE CONFIGS
+document.addEventListener("click", () => {
+    configMenuBox.classList.remove('active')
+})
+
+//MUDA O TEMA
+changeTema();
+
+//ATUALIZA META DE TREINOS
+document.addEventListener('change', (e) => {
+    if(e.target.classList.contains("metaTreino")) {
+        const dia = e.target.closest(".dayCard").dataset.day
+        const barra = e.target.closest(".dayCard").querySelector(".barraTreino")
+        const meta = e.target.value
+    
+        const sec_meta = attMetaDia(dia, meta)
+
+        console.log(sec_meta)
+
+        barra.max = sec_meta
+    }
+})
+
+//INICIAR/PAUSAR TIMER
+let time
+
+document.addEventListener('click', (e) => {
+    if(e.target.classList.contains("timerButton")) {
+        const dia = e.target.closest(".dayCard").dataset.day
+        const barra = e.target.closest(".dayCard").querySelector(".barraTreino")
+        const meta = rotinaExercicios.find(r => r.dia == dia).meta_exercicio
+        const contador = e.target.closest(".dayCard").querySelector(".contador")
+        const objDia = rotinaExercicios.find(d => d.dia == dia)
+
+        if(!e.target.classList.contains("pausa")) {
+            time = startTimer(objDia, barra, meta, contador);
+        } else {
+            time = pauseTimer(objDia)
+        }
+
+        if(!time && time != 0) {
+            e.target.classList.remove("inicia")
+            e.target.classList.add("pausa")
+            e.target.textContent = "Pausar"
+        } else if (time && time == 1) {
+            e.target.classList.remove("pausa")
+            e.target.classList.add("inicia")
+            e.target.textContent = "Retomar"
+        }
+    }
+})
+
+//ADICIONA META DE AGUA E ATUALIZA QUANTIDADE BEBIDO
+document.addEventListener('change', (e) => {
+    const dia = e.target.closest(".dayCard").dataset.day
+    const objDia = rotinaExercicios.find(d => d.dia == dia)
+    if (e.target.classList.contains("inserirMetaAgua")) {
+        objDia.meta_agua = e.target.value
+        e.target.closest(".dayCard").querySelector(".barraAgua").max = e.target.value
+    }
+
+    if (e.target.classList.contains("inserirMlAgua")) {
+        if(objDia.meta_agua <= 0) {
+            alert("Defina uma meta de agua para o dia")
+            e.target.value = 0
+            return
+        }
+        objDia.ml_bebidos = e.target.value
+        e.target.closest(".dayCard").querySelector(".barraAgua").value = e.target.value
+
+    }
+})
+//=============FUNÇÕES=================================//
+
+function formatTime(seconds) {
+    const hours = Math.floor(seconds / 3600)
+    const minutes = Math.floor((seconds % 3600) / 60)
+    const secs = seconds % 60
+
+    return `${String(hours).padStart(2, '0')}:${
+        String(minutes).padStart(2, '0')
+    }:${
+        String(secs).padStart(2, '0')
+    }`
+}
+
+function startTimer(diaObj, progress, totalSeconds, display){
+
+ if(diaObj.interval) return;
+
+ if(diaObj.meta_exercicio <= 0) {
+    alert("Defina um tempo de treino primeiro")
+    return 0
+ }
+
+ let start = Date.now() - diaObj.tempo_feito *1000;
+
+ diaObj.interval = setInterval(()=>{
+
+   let elapsed = Math.floor(
+      (Date.now()-start)/1000
+   );
+
+   diaObj.tempo_feito = elapsed;
+
+   progress.value = elapsed;
+
+   display.textContent =
+      formatTime(elapsed);
+
+ },1000);
+}
+
+function pauseTimer(diaObj){
+
+ clearInterval(diaObj.interval);
+
+ diaObj.interval = null;
+
+ return 1
+}
+
+function changeTema() {
+    const selectTema = document.getElementById('selectTema');
+
+    selectTema.addEventListener('change', () => {
+        const value = selectTema.value;
+        const bodyClass = document.body.classList
+
+        if(value == bodyClass) {
+            return
+        } else if (value == "colorido") {
+            document.body.classList.remove("escuro", "claro");
+            document.body.classList.add("colorido")
+        } else if (value == "claro") {
+            document.body.classList.remove("escuro", "colorido");
+            document.body.classList.add("claro")
+        } else if (value == "escuro") {
+            document.body.classList.remove("colido", "claro");
+            document.body.classList.add("escuro")
+        }
+    })
+}
+
 //ATUALIZA META DO TREINO DO DIA
 function attMetaDia(dia, meta) {
-    const diaSemana = rotinaExercicios.find(r => r.dia == dia)
-    diaSemana.meta_exercicio = meta
+    console.log(meta);
+
+    const diaSemana = rotinaExercicios.find(r => r.dia == dia);
+
+    const sec = timeToSeconds(meta);
+
+    console.log(sec);
+
+    diaSemana.meta_exercicio = sec;
+
+    return sec;
+}
+
+//CONVERTE O VALOR QUE VEM DO INPUT DE TIME PARA SEGUNDOS
+function timeToSeconds(time) {
+    if (!time) return 0;
+
+    const [h, m] = time.split(":").map(Number);
+
+    if (isNaN(h) || isNaN(m)) return 0;
+
+    return (h * 3600) + (m * 60);
 }
 
 //ATUALIZA HORA DO TREINO DO DIA
@@ -104,3 +290,17 @@ function attTempoFeitoDia(dia, tempoFeito) {
     diaSemana.tempo_feito = tempoFeito
 }
 
+//ATUALIZA MLS DE ÁGUA
+function attMlsAgua(dia, mls) {
+    const diaSemana = rotinaExercicios.find(r => r.dia == dia)
+    
+    if (diaSemana.meta_agua == 0) {
+        return
+    }
+
+    const percentualBebido = (mls / diaSemana.meta_agua) * 100
+    console.log(percentualBebido)
+    diaSemana.ml_bebidos = percentualBebido
+
+    return percentualBebido;
+}
